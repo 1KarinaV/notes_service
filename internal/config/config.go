b758ed9815340f0a061e028215f6a422ba30644d
1KarinaV <red@itmo.ru>
2024-04-28 23:56:39 +0300

    feat: api postman and docker files

--- internal/config/config.go
+++ internal/config/config.go
@@ -5,7 +5,7 @@ import "github.com/caarlos0/env/v6"
 type Config struct {
 	Port          string `env:"PORT" envDefault:"9000"`
 	DriverName    string `env:"DRIVER_NAME" envDefault:"postgres"`
-	DbConnect     string `env:"DB_CONNECT" envDefault:"host=localhost user=postgres password=postgres dbname=postgres port=5432 sslmode=disable"`
+	DbConnect     string `env:"DB_CONNECT" envDefault:"host=psql user=postgres password=postgres dbname=postgres port=5432 sslmode=disable"`
 	SecretKey     string `env:"SECRET_KEY" envDefault:"hash"`
 	TokenLifetime int    `env:"TOKEN_LIFETIME" envDefault:"2"`
 	HashCost      int    `env:"HASH_COST" envDefault:"6"`
